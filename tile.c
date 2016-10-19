7dc229056b5e45592e8e0672a8b785dd67a62686
JPisaBrony <jdpohl789@gmail.com>
2016-10-19 10:23:25 -0500

    + missing comment in tile.h

--- tile.c
+++ tile.c
@@ -5,6 +5,7 @@
 // bgcolor is the background color of the tile
 // fgcolor is the forground color of the tile
 // data is the character array of tile
+// name is the name of the tile
 tile* create_tile(int bgcolor, int fgcolor, char data[TILE_SIZE][TILE_SIZE], char *name) {
     tile *cur_tile = malloc(sizeof(tile));
     cur_tile->bgcolor = bgcolor;
